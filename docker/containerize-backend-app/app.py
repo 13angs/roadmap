c2ac82c3cecdb890f46f883472e3a90082659231
13angs <103742354+13angs@users.noreply.github.com>
2025-04-25 13:43:30 +0700

    Update app.py

--- docker/containerize-backend-app/app.py
+++ docker/containerize-backend-app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    NAME = os.getenv('NAME', 'World')
+    NAME = os.getenv('NAME', 'Worlds')
     response = {'message': f'Hello, {NAME}!'}
     return jsonify(response)
 
